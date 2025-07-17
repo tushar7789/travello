7b1d63f4a69652655fa182f42314292a446aee0f
tushar7789 <tusharjangid7@gmail.com>
2025-07-17 22:00:33 +0530

    decided on bg colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './style.css';
+import { url } from 'inspector';
 
 const Header = () => {
     return (
         <div className='header-container'>
-            <div>
+            <div className='first-container'>
                 Welcome to, &nbsp;
             </div>
-            <div>
+            <div className='second-container'>
                 Travello
             </div>
         </div>
