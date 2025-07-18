af68c1d84d1917147493564e302143cb105e4d42
tushar7789 <tusharjangid7@gmail.com>
2025-07-18 10:59:54 +0530

    created static structure for searchbar component

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,38 @@
+'use client'
+
 import React from 'react'
 import './style.css'
 
 const SearchBar = () => {
+    const n: number = 15;
+
+    const handleSubmit = (e: any) => {
+        e.preventDefalut();
+    }
+
     return (
         <div className='searchbar-container'>
-            <div>
-                Search bar will come here
+            <div className='sb-select-container'>
+                <select name="cars" id="cars">
+                    {
+                        Array.from({ length: n }, (_, index) => index + 1).map((num) => (
+                            <option value={num} key={num}>{num}</option>
+                        ))
+                    }
+                </select>
+            </div>
+            <div className='sb-input-container'>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        placeholder='add item'
+                        style={{
+                            marginRight: '20px',
+                            width: '300px'
+                        }}
+                    />
+                    <input type='submit' value='Add item' />
+                </form>
             </div>
         </div>
     )
