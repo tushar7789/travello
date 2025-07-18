fff06d3abea63bd8636242ea3d1f46f0928ba9cc
tushar7789 <tusharjangid7@gmail.com>
2025-07-18 12:35:11 +0530

    created static structure for content component

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,12 +1,52 @@
 import React from 'react'
 import './style.css'
 
+let itemList = [
+    {
+        "item": "sock",
+        "amount": 3,
+        "checked": false
+    },
+    {
+        "item": "shirt",
+        "amount": 5,
+        "checked": false
+    }
+]
+
+const itemCapacityPerBox = 4;
+
 const Content = () => {
+    const nOfBox = Math.round(itemList.length / itemCapacityPerBox);
+    const itemsOnLastBox = itemList.length % itemCapacityPerBox;
+
     return (
         <div className='content-container'>
-            <div>
-                The content will come here!
-            </div>
+            {
+                Array.from({ length: nOfBox }, (_, index: any) => (
+                    <div className='c-box-container' key={index}>
+                        {
+                            itemList
+                                .slice(index * 4, index * 4 + ((index + 1 === nOfBox) ? itemsOnLastBox : itemCapacityPerBox))
+                                .map((ele: any, ind: any) => (
+                                    <div className='c-item-container'>
+                                        <input type='checkbox' />
+                                        <span
+                                            style={{
+                                                marginLeft: '10px'
+                                            }}
+                                        >
+                                            {ele['item']}
+                                            &nbsp;&nbsp;&nbsp;
+                                            <i>{ele['amount']}</i>
+                                        </span>
+                                    </div>
+                                ))
+                        }
+
+                    </div>
+                ))
+            }
         </div>
     )
 }
